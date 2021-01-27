66cf321031f8f09d0f3f58d55a999e55138fec1b
Aschen <adrien@maret.email>
2021-01-27 07:27:39 +0100

    wip

--- docker/scripts/functional-tests-controller.ts
+++ docker/scripts/functional-tests-controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Request } from '../../index';
+import { Controller, KuzzleRequest } from '../../index';
 
 export class FunctionalTestsController extends Controller {
   constructor (app) {
@@ -7,21 +7,37 @@ export class FunctionalTestsController extends Controller {
     this.definition = {
       actions: {
         helloWorld: {
-          handler: this.helloWorld
+          handler: this.helloWorld,
         },
         byeWorld: {
           handler: this.byeWorld
+        },
+        create: {
+          handler: this.create,
+          args: [
+            { name: 'index', type: 'string' },
+            { name: 'collection', type: 'string' },
+            { name: 'body', type: 'JSONObject' },
+          ],
+          opts: {
+            _id: 'string',
+            refresh: 'string',
+          }
         }
       }
     };
   }
 
-  async helloWorld (request: Request) {
+  async helloWorld (request: KuzzleRequest) {
     return { greeting: `Hello, ${request.input.args.name}` };
   }
 
   async byeWorld () {
     // ensure the "app" property is usable
     return this.app.sdk.document.create('test', 'test', { message: 'bye' });
+  }
+
+  async create (request: KuzzleRequest) {
+
   }
 }
